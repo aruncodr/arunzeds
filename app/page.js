210d7ad1824b8cdda22536ea989561494dc70674
aruncodr <aruncodr@gmail.com>
2024-09-23 18:13:50 +0530

    name

--- app/page.js
+++ app/page.js
@@ -3,7 +3,18 @@ import Image from "next/image";
 export default function Home() {
   return (
 <div>
-  Hi arun
+
+  <div className="flex pt-36">
+    <div className="w-1/2 pl-24">
+      <div className="text-[64px]">I'm arun</div>
+      <div className="text-[32px]">Front end developer</div>
+    </div>
+
+    <div className="w-1/2">
+    Image
+    </div>
+  </div>
+
 </div>
   );
 }
