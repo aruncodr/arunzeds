57a335cacf8f310f58d1789be5f39644dd04e2a1
aruncodr <aruncodr@gmail.com>
2024-09-25 16:41:01 +0530

    COMPLETE

--- app/page.js
+++ app/page.js
@@ -1,11 +1,19 @@
+'use client'
+ import { useRouter } from 'next/navigation'
 import Image from "next/image";
-import PageContent from "./CV/PageContent";
+import PageContent from "./cv/page";
 
 
 export default function Home() {
+  const router = useRouter()
+
   return (
 <div>
-    <PageContent />
+  <div className=''>
+    <button type="button" onClick={() => router.push('./cv/')}>
+      Show My CV
+    </button>
+    </div>
 </div>
   );
 }
