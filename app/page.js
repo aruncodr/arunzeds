b56e697b98e3a69d0896bfe4604f9f686c5926a0
aruncodr <aruncodr@gmail.com>
2024-09-29 01:18:56 +0530

    cv new added

--- app/page.js
+++ app/page.js
@@ -1,19 +1,20 @@
 'use client'
  import { useRouter } from 'next/navigation'
 import Image from "next/image";
-import PageContent from "./cv/page";
+import PageContent from "./cv0/page";
 
 
 export default function Home() {
   const router = useRouter()
 
   return (
-<div>
-  <div className=''>
-    <button type="button" onClick={() => router.push('./cv/')}>
+<div className='bg-white flex flex-col'>
+    <button className='h-64 w- bg-teal-500' type="button" onClick={() => router.push('./cv/')}>
       Show My CV
     </button>
-    </div>
+    <button className='h-64 w- bg-blue-500' type="button" onClick={() => router.push('./cv0/')}>
+      Show My CV
+    </button>
 </div>
   );
 }
