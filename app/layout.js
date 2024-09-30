2bbd24953776901821b87de0b3597e7dfccf3473
aruncodr <aruncodr@gmail.com>
2024-09-30 12:18:34 +0530

    meta data

--- app/layout.js
+++ app/layout.js
@@ -6,8 +6,8 @@ import './globals.css'
 const inter = Raleway({ subsets: ['latin']})
 
 export const metadata = {
-  title: 'Arun Zeds',
-  description: 'Arun Zeds',
+  title: 'Portfolio | arun zeds ',
+  description: "Hi, I’m Arun Zeds  a web developer with a passion for crafting innovative website with great create user experience. Outside of coding, I love photography and exploring men's lifestyles. Let’s connect!",
 }
 
 export default function RootLayout({ children }) {
