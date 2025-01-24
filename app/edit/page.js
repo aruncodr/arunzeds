0c8aa709f9419de4642dfeb4d05bbbaf8c343d91
aruncodr <aruncodr@gmail.com>
2025-01-24 16:20:10 +0530

    database addded

--- app/edit/page.js
+++ app/edit/page.js
@@ -1,9 +1,30 @@
+import TopicsList from '@/components/BlogsList'
+import Link from 'next/link'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-        hai arun
+    <div className='px-4 lg:px-64 mt-10'>
+
+        <div className='pb-4 flex justify-between items-center'>
+          <Link className="bg-green-900 text-white p-2 px-6 rounded-lg" href={"/edit"}>
+            Refresh
+          </Link>
+          <Link className="bg-green-900 text-white p-2 px-6 rounded-lg" href={"/addBlog"}>
+            Add Blog
+          </Link>
+        </div>
+        <TopicsList/>
+
+        <div className='pt-4 flex justify-between items-center'>
+          <Link className="bg-green-900 text-white p-2 px-6 rounded-lg" href={"/edit"}>
+            Refresh
+          </Link>
+          <Link className="bg-green-900 text-white p-2 px-6 rounded-lg" href={"/addBlog"}>
+            Add Blog
+          </Link>
+        </div>
+
     </div>
   )
 }
---
+++ app/editBlog/[id]/page.jsx
@@ -0,0 +1,25 @@
+import EditBlogForm from "@/components/EditBlogForm";
+
+const getBlogById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/blogs/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch blog");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default async function EditBlog({ params }) {
+  const { id } = await  params;
+  const { blog } = await getBlogById(id);
+  const { title, description } = blog;
+
+  return <EditBlogForm id={id} title={title} description={description} />;
+}
