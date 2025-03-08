4e94c9ad3818d8b62ca4a0343872197fc82670eb
aruncodr <aruncodr@gmail.com>
2025-03-08 19:11:27 +0530

    arun

--- app/page.js
+++ app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
     <div className='flex h-screen lg:h-[640px]'>
 
     <div className=''>
-    <img className='absolute hidden z-[-2] -bottom-4 -right-36 lg:right-44 w-[900px] lg:w-[760px]' src="/aruncrop.svg"></img>
+    <img className='absolute hidden z-[-2] -bottom-4 -right-36 lg:right-44 w-[900px] lg:w-[760px]' src="/aruncrop.svg" alt='arun zed' title='arun zed'></img>
 
     </div>
     {/* <div className="bg-[url('/aruncorp.svg')]"></div> */}
@@ -255,6 +255,11 @@ export default function Home() {
         <a target="_blank" rel="noopener noreferrer" href='https://instagram.com/arun_zed_'>
           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 64 64">
           <path fill="#78716c" d="M 21.580078 7 C 13.541078 7 7 13.544938 7 21.585938 L 7 42.417969 C 7 50.457969 13.544938 57 21.585938 57 L 42.417969 57 C 50.457969 57 57 50.455062 57 42.414062 L 57 21.580078 C 57 13.541078 50.455062 7 42.414062 7 L 21.580078 7 z M 47 15 C 48.104 15 49 15.896 49 17 C 49 18.104 48.104 19 47 19 C 45.896 19 45 18.104 45 17 C 45 15.896 45.896 15 47 15 z M 32 19 C 39.17 19 45 24.83 45 32 C 45 39.17 39.169 45 32 45 C 24.83 45 19 39.169 19 32 C 19 24.831 24.83 19 32 19 z M 32 23 C 27.029 23 23 27.029 23 32 C 23 36.971 27.029 41 32 41 C 36.971 41 41 36.971 41 32 C 41 27.029 36.971 23 32 23 z"></path>
+          </svg>
+          </a>
+        <a className=' text-white' target="_blank" rel="noopener noreferrer" href='https://arunzeds.com/arunzed'>
+          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 64 64">
+          <path fill="#ffffff" d="M 21.580078 7 C 13.541078 7 7 13.544938 7 21.585938 L 7 42.417969 C 7 50.457969 13.544938 57 21.585938 57 L 42.417969 57 C 50.457969 57 57 50.455062 57 42.414062 L 57 21.580078 C 57 13.541078 50.455062 7 42.414062 7 L 21.580078 7 z M 47 15 C 48.104 15 49 15.896 49 17 C 49 18.104 48.104 19 47 19 C 45.896 19 45 18.104 45 17 C 45 15.896 45.896 15 47 15 z M 32 19 C 39.17 19 45 24.83 45 32 C 45 39.17 39.169 45 32 45 C 24.83 45 19 39.169 19 32 C 19 24.831 24.83 19 32 19 z M 32 23 C 27.029 23 23 27.029 23 32 C 23 36.971 27.029 41 32 41 C 36.971 41 41 36.971 41 32 C 41 27.029 36.971 23 32 23 z"></path>
           </svg>
           </a>
       </div>
